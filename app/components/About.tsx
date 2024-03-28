12ff9af50dd16939233d38e5cc7af48a2e4e57d8
Vedant <vedant.sawant.2604@gmail.com>
2024-03-28 16:31:10 -0400

    adds framer motion transitions

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { motion } from 'framer-motion'
 import Button from './Button'
 import Image from 'next/image'
 
@@ -7,30 +8,131 @@ export default function About() {
     <section id="about" className="bg-white item py-10 content">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
-          <p className="max-w-2xl mb-6 font-light font-syne text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
-            Hi, my name is Vedant
-          </p>
-          <h1 className="max-w-2xl mb-4 text-6xl font-archivo font-black tracking-tight leading-none md:text-7xl xl:text-8xl">
-            I BUILD WEB APPS
-          </h1>
-          <p className="max-w-2xl mb-6 font-light font-syne text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
-            From Full stack applications to Distributed Systems.
-          </p>
-          <Button
-            onClick={() => {
-              console.log('btn click')
+          <motion.div
+            initial={{
+              opacity: 0,
+              x: 30,
+            }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+            }}
+            viewport={{
+              once: true,
+              amount: 1,
+            }}
+            transition={{
+              type: 'tween',
+              duration: 0.5,
+              delay: 0.1,
             }}
           >
-            Resume
-          </Button>
+            <p className="max-w-2xl mb-4 font-light font-syne text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
+              Hi, my name is Vedant
+            </p>
+          </motion.div>
+          <motion.div
+            initial={{
+              opacity: 0,
+              x: 30,
+            }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+            }}
+            viewport={{
+              once: true,
+              amount: 1,
+            }}
+            transition={{
+              type: 'tween',
+              duration: 0.5,
+              delay: 0.5,
+            }}
+          >
+            {/* Projects I&apos;ve worked on */}
+            <h1 className="max-w-2xl mb-4 text-6xl font-archivo font-black tracking-tight leading-none md:text-7xl xl:text-8xl">
+              I BUILD WEB APPS
+            </h1>
+          </motion.div>
+
+          <motion.div
+            initial={{
+              opacity: 0,
+              x: 30,
+            }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+            }}
+            viewport={{
+              once: true,
+              amount: 1,
+            }}
+            transition={{
+              type: 'tween',
+              duration: 0.5,
+              delay: 0.1,
+            }}
+          >
+            <p className="max-w-2xl mb-4 font-light font-syne text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
+              From Full stack applications to Distributed Systems.
+            </p>
+          </motion.div>
+          <motion.div
+            initial={{
+              opacity: 0,
+              y: 10,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            viewport={{
+              once: true,
+              amount: 1,
+            }}
+            transition={{
+              type: 'spring',
+              duration: 0.5,
+              delay: 0.5,
+            }}
+          >
+            <Button
+              onClick={() => {
+                console.log('btn click')
+              }}
+            >
+              Resume
+            </Button>
+          </motion.div>
         </div>
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: 30,
+          }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+          }}
+          viewport={{
+            once: true,
+            amount: 1,
+          }}
+          transition={{
+            type: 'spring',
+            duration: 0.5,
+            delay: 0.5,
+          }}
+          className="hidden lg:mt-0 lg:col-span-5 lg:flex"
+        >
           <img
             src="/Vedant_Sawant.jpg"
             alt="Hero"
             className="object-cover rounded-lg border-2 border-black shadow-[6px_6px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[-3px] hover:translate-y-[-3px] hover:shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] hover:grayscale"
           />
-        </div>
+        </motion.div>
       </div>
     </section>
   )
